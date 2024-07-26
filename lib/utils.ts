c44f68e7e2c466ce4d461374b35a708b7d3ea2ee
Irhan M <irhanmustofa28@gmail.com>
2024-07-26 23:30:20 +0700

    bissmillah

---
+++ lib/utils.ts
@@ -0,0 +1,47 @@
+export const formateDate = (date: string) => {
+    const formattedDate = new Date(date);
+    const formatter = new Intl.DateTimeFormat("id-ID", {
+        dateStyle: "medium",
+        timeStyle: "short",
+    });
+    return formatter.format(formattedDate);
+}
+
+export const generatePagination = (currentPage: number, totalPages: number) => {
+    if (totalPages <=7) {
+        return Array.from({ length: totalPages }, (_, i) => i + 1);
+    }
+
+    if (currentPage <= 3) {
+        return [
+            1,
+            2,
+            3,
+            "...",
+            totalPages - 1,
+            totalPages
+        ];
+    }
+
+    if (currentPage >= totalPages - 2) {
+        return [
+            1,
+            2,
+            3,
+            "...",
+            totalPages - 2,
+            totalPages - 1,
+            totalPages
+        ];
+    }
+
+    return[
+        1,
+        "...",
+        currentPage - 1,
+        currentPage,
+        currentPage + 1,
+        "...",
+        totalPages
+    ]
+}
